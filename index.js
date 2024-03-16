3433438d85f338641500324be087aba3380716c7
xxasimovxx <asimovpl@wp.pl>
2024-03-17 00:10:23 +0100

    png-ascii, file loss

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-var fs = require("fs"),
+const fs = require("fs");
+var im = require('imagemagick');
 
+PNG = require("pngjs").PNG;
+function Resize(){
+ im.resize({
+  srcPath:  'asd.png',
+  dstPath:  'example.png',
+  width:200
+}, function(err, stdout, stderr){
+  if (err) throw err
+  console.log('resized')
+});}
 
-  PNG = require("pngjs").PNG;
+let promise = new Promise(function(resolve,reject){
+  Resize()
+  if(fs.existsSync("example.png")){
+    resolve("data")
+  }
+  reject(new Error("adadasd"))
 
-fs.createReadStream("example.png")
+})
+
+makeImg()
+async function makeImg(){
+await promise.then(fs.createReadStream("example.png")
   .pipe(
     new PNG({
       filterType: 4,
@@ -15,52 +35,30 @@ fs.createReadStream("example.png")
       string += "\n";
       for (var x = 0; x < this.width; x++) {
         //if (x % 2 == 0 && y % 2 == 0) {
-          string += "  ";
-          var idx = (this.width * y + x) << 2;
-          const brightness = (this.data[idx] + this.data[idx + 1] + this.data[idx+2])/3
-          //const brightness =
-          //  (this.data[idx] +
-          //    this.data[idx + 1] +
-          //    this.data[idx + 2] +
-          //    this.data[idx + 3] +
-          //    this.data[idx + 4] +
-          //    this.data[idx + 5] +
-          //    this.data[idx + this.width] +
-          //    this.data[idx + 1 + this.width] +
-          //    this.data[idx + 2 + this.width] +
-          //    this.data[idx + 3 + this.width] +
-          //    this.data[idx + 4 + this.width] +
-          //    this.data[idx + 5 + this.width]) /
-          //  12;
+        string += "  ";
+        var idx = (this.width * y + x) << 2;
+        const brightness =
+          (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
 
-          if (brightness < 36) {
-            string += "W";
-          } else if (brightness < 72) {
-            string += "w";
-          } else if (brightness < 108) {
-            string += "l";
-          } else if (brightness < 144) {
-            string += "i";
-          } else if (brightness < 180) {
-            string += ":";
-          } else if (brightness < 216) {
-            string += ",";
-          } else {
-            string += ".";
-          }
+        if (brightness < 36) {
+          string += "W";
+        } else if (brightness < 72) {
+          string += "w";
+        } else if (brightness < 108) {
+          string += "l";
+        } else if (brightness < 144) {
+          string += "i";
+        } else if (brightness < 180) {
+          string += ":";
+        } else if (brightness < 216) {
+          string += ",";
+        } else {
+          string += ".";
         }
-
-        // invert color
-        //        this.data[idx] = 255 - this.data[idx];
-        //        this.data[idx + 1] = 255 - this.data[idx + 1];
-        //        this.data[idx + 2] = 255 - this.data[idx + 2];
-
-        // and reduce opacity
-        //this.data[idx + 3] = this.data[idx + 3] >> 1;
       }
-    //}
 
+    }
     fs.writeFile("out.txt", string, function(err) {
       if (err) throw err;
     });
-  });
+  }),error => console.log(error))}
