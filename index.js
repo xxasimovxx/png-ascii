a40b3e697363aa1421802a2f133f51a35a4fb8c9
xxasimovxx <asimovpl@wp.pl>
2024-03-18 16:25:28 +0100

    png-ascii

--- index.js
+++ index.js
@@ -1,72 +1,58 @@
-const fs = require("fs");
+var fs = require("fs");
+var path = require('path');
+PNG = require("pngjs").PNG;
 const sharp = require("sharp");
 
-PNG = require("pngjs").PNG;
-//sharp("asd.png")
-//  .resize({ width: 200 })
-//  .toFile("example.png")
-//  .then(function (newFileInfo) {
-//    // newFileInfo holds the output file properties
-//    console.log("Success");
-//  })
-//  .catch(function (err) {
-//    console.log(err);
-//  });
-async function resizeImg(){
-await sharp("asd.png")
-  .resize({ width: 200 })
-  .png()
-  .toBuffer()
-  .then((data) => {
-    fs.writeFile("example.png", data)
-      .then(() => {
-        console.log("asd");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  })
-  .catch((err) => console.log(err));
-//while(!fs.existsSync("example.png")){
+const promise = fs.promises.readFile(path.join('asd.png'));
 
-//  if(fs.existsSync("example.png")){break}
-//}
+Promise.resolve(promise).then(function(buffer){
+     resizeImage(buffer)
+});
+
+let string = "";
+async function resizeImage(buffer) {
+    await sharp(buffer).resize(200).toFile("example.png");
+    makeImg(string)
+
+       fs.writeFileSync("out.txt", string, err => {
+         console.log(err +"asd");
+       });
 }
-resizeImg()
-fs.createReadStream("example.png")
-  .pipe(
-    new PNG({
-      filterType: 4,
-    }),
-  )
-  .on("parsed", function () {
-    let string = "";
-    for (var y = 0; y < this.height; y++) {
-      for (var x = 0; x < this.width; x++) {
-        var idx = (this.width * y + x) << 2;
+function makeImg(string) {
 
-        const brightness =
-          (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
+  fs.createReadStream("example.png")
+    .pipe(
+      new PNG({
+        filterType: 4,
+      }),
+    )
+    .on("parsed", function () {
+      for (var y = 0; y < this.height; y++) {
+        string += "/n"
+        for (var x = 0; x < this.width; x++) {
+          var idx = (this.width * y + x) << 2;
 
-        if (brightness < 36) {
-          string += "W";
-        } else if (brightness < 72) {
-          string += "w";
-        } else if (brightness < 108) {
-          string += "l";
-        } else if (brightness < 144) {
-          string += "i";
-        } else if (brightness < 180) {
-          string += ":";
-        } else if (brightness < 216) {
-          string += ",";
-        } else {
-          string += ".";
+          const brightness =
+            (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
+
+          if (brightness < 36) {
+            string += "W";
+          } else if (brightness < 72) {
+            string += "w";
+          } else if (brightness < 108) {
+            string += "l";
+          } else if (brightness < 144) {
+            string += "i";
+          } else if (brightness < 180) {
+            string += ":";
+          } else if (brightness < 216) {
+            string += ",";
+          } else {
+            string += ".";
+          }
         }
       }
-    }
 
-    fs.writeFileSync("out.txt", string, function (err) {
-      console.log(err);
     });
-  });
+
+}
